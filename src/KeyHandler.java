0773ebafb3189c1c1a278bb5652e7c2935aa3922
Lasa <102794245+Devilasa@users.noreply.github.com>
2023-05-15 21:44:50 +0200

    Add files via upload

---
+++ src/KeyHandler.java
@@ -0,0 +1,33 @@
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyHandler implements KeyListener {
+    public boolean upPressed, downPressed, rightPressed, leftPressed;
+    @Override
+    public void keyTyped(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyPressed(KeyEvent keyEvent) {
+        int code = keyEvent.getKeyCode();
+        switch (code) {
+            case KeyEvent.VK_W -> upPressed = true;
+            case KeyEvent.VK_A -> leftPressed = true;
+            case KeyEvent.VK_S -> downPressed = true;
+            case KeyEvent.VK_D -> rightPressed = true;
+
+        }
+    }
+
+    @Override
+    public void keyReleased(KeyEvent keyEvent) {
+        int code = keyEvent.getKeyCode();
+        switch (code) {
+            case KeyEvent.VK_W -> upPressed = false;
+            case KeyEvent.VK_A -> leftPressed = false;
+            case KeyEvent.VK_S -> downPressed = false;
+            case KeyEvent.VK_D -> rightPressed = false;
+        }
+    }
+}
